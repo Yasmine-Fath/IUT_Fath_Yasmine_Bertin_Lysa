44573e4e0e6ff596384a0aeba8207fc66b5c4b18
iutgeiitoulonE105 <touloniut@gmail.com>
2025-09-18 12:25:15 +0200

    18_09_0.0

--- MPLABX/Embedded/fath-bertin-robot-projet-1.2.X/main.c
+++ MPLABX/Embedded/fath-bertin-robot-projet-1.2.X/main.c
@@ -12,6 +12,8 @@
 
 unsigned int ADCValue0, ADCValue1, ADCValue2;
 
+int N = 2296; // valeur ADC pour une distance de 30cm
+
 int main(void) {
 
     //Initialisation oscillateur
@@ -20,18 +22,18 @@ int main(void) {
     InitTimer23();
     InitTimer1();
 
-    // Configuration des input et output (IO)
-    LED_BLANCHE_1 = 1;
-    LED_BLEUE_1 = 1;
-    LED_ORANGE_1 = 1;
-    LED_ROUGE_1 = 1;
-    LED_VERTE_1 = 1;
-
-    LED_BLANCHE_2 = 1;
-    LED_BLEUE_2 = 1;
-    LED_ORANGE_2 = 1;
-    LED_ROUGE_2 = 1;
-    LED_VERTE_2 = 1;
+//    // Configuration des input et output (IO)
+//    LED_BLANCHE_1 = 1;
+//    LED_BLEUE_1 = 1;
+//    LED_ORANGE_1 = 1;
+//    LED_ROUGE_1 = 1;
+//    LED_VERTE_1 = 1;
+//
+//    LED_BLANCHE_2 = 1;
+//    LED_BLEUE_2 = 1;
+//    LED_ORANGE_2 = 1;
+//    LED_ROUGE_2 = 1;
+//    LED_VERTE_2 = 1;
 
 
     //PWM
@@ -44,7 +46,35 @@ int main(void) {
         if(ADCIsConversionFinished() == 1){
             ADCValue0 = result[0]; 
             ADCValue1 = result[1]; 
-            ADCValue2 = result[2]; 
+            ADCValue2 = result[2];
+            
+//            if (ADCValue0 < N){
+//                LED_BLEUE_1 = 1;
+//            }
+//            else{
+//                LED_BLEUE_1 = 0;
+//            }
+//            if (ADCValue1 < N){
+//                LED_ORANGE_1 = 1;
+//            }
+//            else{
+//                LED_ORANGE_1 = 0;
+//            }
+//            if (ADCValue2 < N){
+//                LED_ROUGE_1 = 1;
+//            }
+//            else{
+//                LED_ROUGE_1 = 0;
+//            }
+            
+            ADCClearConversionFinishedFlag();
+            unsigned int * result = ADCGetResult();
+            float volts = ((float) result [0])* 3.3 / 4096;
+            robotState.distanceTelemetreGauche = 34 / volts - 5;
+            volts = ((float) result [1])* 3.3 / 4096;
+            robotState.distanceTelemetreCentre = 34 / volts - 5;
+            volts = ((float) result [2])* 3.3 / 4096;
+            robotState.distanceTelemetreDroit = 34 / volts - 5;
             
         }
      
