9ca404c1f46627d6e744c7e152f9138f74634475
iutgeiitoulonE105 <touloniut@gmail.com>
2025-10-16 12:28:07 +0200

    #c_16_10_0.1

---
+++ C#/RobotInterface/Robot_interface_fath_bertin/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Robot_interface_fath_bertin
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ C#/RobotInterface/Robot_interface_fath_bertin/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Robot_interface_fath_bertin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        bool toggle;
+        private void buttonEnvoyer_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (toggle) {
+                buttonEnvoyer.Background = Brushes.Beige; 
+            }
+            else
+            {
+                buttonEnvoyer.Background = Brushes.RoyalBlue;
+            }
+            toggle = !toggle;
+        }
+    }
+}
